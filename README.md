7fd2ba97f576da7e1087e134473cd2daf5b5b7ed
Chandi977 <pappumahato000@gmail.com>
2024-07-03 16:07:27 +0530

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,66 @@
 ﻿# SpeakBot
+
+SpeakBot is a voice-controlled assistant application that allows users to interact via speech commands. It integrates various APIs to provide functionalities such as weather updates, web searches, and more.
+
 [For demo click here](https://chandi977.github.io/SpeakBot/home.html)
-Commands :-
-1. Hello, Hi, Hey
+---
+## Commands
+
+SpeakBot currently supports the following commands:
+
+- **Greetings:**
+  - "Hello"
+  - "Hi SpeakBot"
+  - "Hey there"
+
+- **Weather:**
+  - "What's the weather?"
+  - "Tell me the weather in [city]"
+  - "How's the weather today?"
+
+- **Information:**
+  - "Tell me about [topic]"
+  - "Who created you?"
+  - "What can you do for me?"
+
+- **Actions:**
+  - "Open [website]"
+  - "Refresh"
+  - "Add [item]"
+
+- **Miscellaneous:**
+  - "Thank you"
+  - "Nice to meet you"
+  - "What's the date?"
+
+## Technologies Used
+
+SpeakBot is built using the following technologies:
+
+- **HTML/CSS/JavaScript**: Front-end development.
+- **SpeechRecognition API**: Enables voice recognition for commands.
+- **SpeechSynthesis API**: Provides text-to-speech capabilities.
+- **Fetch API**: Handles HTTP requests to external APIs.
+- **Geolocation API**: Retrieves user's current location for weather updates.
+
+## APIs Integrated
+
+SpeakBot currently integrates the following APIs:
+
+- **OpenWeatherMap API**: Provides current weather data based on location.
+- **Google Search API**: Executes web searches for user queries.
+- **Wikipedia API**: Retrieves information from Wikipedia for informational queries.
+
+## Future Updates
+
+For future updates, consider integrating the following APIs and features:
+
+- **News API**: Fetches latest news updates based on user queries.
+- **Calendar API**: Adds events to user's calendar based on voice commands.
+- **AI Chatbot Integration**: Enhances conversational abilities using AI models like OpenAI's GPT.
+- **Personalization**: Customizes responses based on user preferences or historical interactions.
+- **Multi-language Support**: Extends support for multiple languages for global users.
+
+Feel free to contribute to SpeakBot and suggest new features or improvements. Happy speaking!
+
+
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const api_key2 = "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";
+
 function champ() {
   document.getElementById("champ").style.display = "block";
   document.getElementById("mic-btn").style.display = "none";
@@ -7,18 +9,14 @@ function champ() {
     document.getElementById("mic-btn").style.display = "block";
   }, 3000);
 }
+
 const animate = document.querySelectorAll(".animate");
 const chatbox = document.querySelector(".chatbox");
-// const mic = document.getElementById("#mic-btn");
-
 const synth = window.speechSynthesis;
 const amISpeaking = synth.speaking;
 synth.voice = "hi-IN";
 let replay = "";
 const api_key = "5878944582ef8963d92960f95ef05d29";
-
-// require('dotenv').config();
-// console.log(process.env);
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -29,45 +27,28 @@ function speak(replay) {
   speechSynthesis.speak(new SpeechSynthesisUtterance(replay));
   console.log(!amISpeaking);
 }
-// replay = "Hi, I am a SpeakBot. How can i help you";
-// chatbox.innerHTML +=
-//   '<div class="eachmessage received animated"><p>' + replay + "</p></div>";
-// chatbox.scrollTop = chatbox.scrollHeight;
-// speak(replay);
-
 recognition.addEventListener("result", (e) => {
+  let p = document.getElementById("p");
   replay = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
     .join("");
-  // chatbox.innerHTML += '<div class="eachmessage sent animated"><p>' + replay + '</p></div>';
 
   const finalText = replay.toUpperCase();
+
   if (e.results[0].isFinal) {
-    chatbox.innerHTML +=
-      '<div class="eachmessage sent animated"><p>' + replay + "</p></div>";
-    // the recognition AI......
     if (finalText.includes("HELLO" || "HI" || "HEY" || "HEY SPEAKBOT")) {
-      replay = "Hi, I am a SpeakBot. How can i help you";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      replay = "Hi, I am a SpeakBot. How can I help you?";
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("HOW ARE YOU")) {
-      replay = "I am fine, Thankyou.";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      replay = "I am fine, Thank you.";
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("WHAT'S THE WEATHER")) {
-      const current_city = getCurrentCity()
+      getCurrentCity()
         .then((cityName) => {
           console.log("Current City:", cityName);
-
           const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}`;
 
           fetch(url)
@@ -77,41 +58,28 @@ recognition.addEventListener("result", (e) => {
             .then(function (data) {
               const temp = data.main.temp - 273.15;
               const text = `The weather in ${cityName} is`;
-              const replay = `${text} ${Math.trunc(temp)}° celcius.`;
-              chatbox.innerHTML +=
-                '<div class="eachmessage received animated"><p>' +
-                replay +
-                "</p> </div>";
-              chatbox.scrollTop = chatbox.scrollHeight;
+              replay = `${text} ${Math.trunc(temp)}° Celsius.`;
+              addMessageToChat(replay, "received");
               speak(replay);
             })
             .catch(function (err) {
               console.error(err);
-              const replay = "Sorry, I cannot find the city.";
-              chatbox.innerHTML +=
-                '<div class="eachmessage received animated"><p>' +
-                replay +
-                "</p> </div>";
-              chatbox.scrollTop = chatbox.scrollHeight;
+              replay = "Sorry, I cannot find the city.";
+              addMessageToChat(replay, "received");
               speak(replay);
             });
         })
         .catch((error) => {
           console.error("Error getting current city:", error);
-          const replay = "Sorry, I cannot determine the current city.";
-          chatbox.innerHTML +=
-            '<div class="eachmessage received animated"><p>' +
-            replay +
-            "</p> </div>";
-          chatbox.scrollTop = chatbox.scrollHeight;
+          replay = "Sorry, I cannot determine the current city.";
+          addMessageToChat(replay, "received");
           speak(replay);
         });
     } else if (finalText.includes("CAN YOU TELL ME THE WEATHER OF")) {
       let temp = finalText.search("TELL ME ABOUT");
-
       let query = finalText.substr(temp + 31, finalText.length);
       query = query.trim();
-      console.log(query);
+
       if (query != "") {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${api_key}`;
         fetch(url)
@@ -121,201 +89,142 @@ recognition.addEventListener("result", (e) => {
           .then(function (data) {
             temp = data.main.temp - 273.15;
             const text = "The weather in " + query + " is";
-            replay = text + " " + Math.trunc(temp) + "° celcius.";
-            chatbox.innerHTML +=
-              '<div class="eachmessage received animated"><p>' +
-              replay +
-              "</p> </div>";
-            chatbox.scrollTop = chatbox.scrollHeight;
+            replay = `${text} ${Math.trunc(temp)}° Celsius.`;
+            addMessageToChat(replay, "received");
             speak(replay);
           })
           .catch(function (err) {
-            // console.log(err);
-            replay = "Sorry I cannot find The city.";
-            chatbox.innerHTML +=
-              '<div class="eachmessage received animated"><p>' +
-              replay +
-              "</p> </div>";
-            chatbox.scrollTop = chatbox.scrollHeight;
+            replay = "Sorry, I cannot find the city.";
+            addMessageToChat(replay, "received");
             speak(replay);
           });
       } else {
         replay = "Please tell me the city name.";
-        chatbox.innerHTML +=
-          '<div class="eachmessage received animated"><p>' +
-          replay +
-          "</p></div>";
-        chatbox.scrollTop = chatbox.scrollHeight;
+        addMessageToChat(replay, "received");
         speak(replay);
       }
     } else if (finalText.includes("THANK YOU")) {
       replay = "It's my pleasure to help you.";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "&#x1F60A;</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (
       finalText.includes("WHAT WILL YOU DO FOR ME" || "WHAT CAN YOU DO FOR ME")
     ) {
       replay =
-        "I can run applications, search any text in wikipedia, can give weather update etc.";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+        "I can run applications, search any text in Wikipedia, and provide weather updates, etc.";
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("NICE TO MEET YOU")) {
-      replay = "NICE TO MEET YOU TO.";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      replay = "Nice to meet you too.";
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("NICE")) {
       replay = "Nice to";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>&#128519;Nice to.</p></div>';
-      chatbox.scrollTop = chatbox.scrollHeight;
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("TELL ME ABOUT")) {
       let temp = finalText.search("TELL ME ABOUT");
       let query = finalText.substr(temp + 13, finalText.length);
       query = query.trim();
 
-      function getResults(query) {
-        const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&origin=*&srlimit=25&utf8=&format=json&srsearch=${query}`;
-        fetch(url)
-          .then((res) => res.json())
-          .then((data) => {
-            putResults(data.query.search);
-          })
-          .catch((e) => console.log(`ERROR : ${e}`));
-      }
-
-      function putResults(sResults) {
-        let count = 0;
-        sResults.forEach((result) => {
-          count += 1;
-          if (count == 1) {
-            const url = encodeURI(
-              `https://en.wikipedia.org/wiki/${result.title}`
-            );
-            console.log(removeTags(result.snippet));
-
-            function removeTags(str) {
-              if (str === null || str === "") return false;
-              else {
-                str = str.toString();
-                replay = str.replace(/(<([^>]+)>)/gi, "");
-                chatbox.innerHTML +=
-                  '<div class="eachmessage received animated"><p>' +
-                  replay +
-                  "</p></div>";
-                chatbox.scrollTop = chatbox.scrollHeight;
-                speak(replay);
-              }
-              return str.replace(/(<([^>]+)>)/gi, "");
-            }
-          } else {
-            return false;
-          }
-        });
-      }
-      getResults(query);
+      generateResponse(messageElement.querySelector("p"), query); // Call generateResponse with appropriate messageElement
     } else if (finalText.includes("WHAT'S YOUR NAME" || "WHO ARE YOU")) {
-      replay = "My Name is SpeakBot";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      replay = "My name is SpeakBot.";
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("ADD")) {
       let temp = finalText.search("ADD");
       let query = finalText.substr(temp + 3, finalText.length);
       query = query.trim();
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("WHAT'S THE DATE")) {
-      var today = new Date();
-      var dd = String(today.getDate()).padStart(2, "0");
-      var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-      var yyyy = today.getFullYear();
-      today = mm + "/" + dd + "/" + yyyy;
+      const today = new Date();
+      const dd = String(today.getDate()).padStart(2, "0");
+      const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+      const yyyy = today.getFullYear();
+      today = mm + "/" + dd + "/";
       replay = today;
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("WHO CREATED YOU")) {
       replay = "Chandi Charan Mahato";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("OPEN")) {
       let temp = finalText.search("OPEN");
       let query = finalText.substr(temp + 4, finalText.length);
       query = query.trim();
       replay = `Opening ${query}..`;
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      addMessageToChat(replay, "received");
       speak(replay);
       console.log(query);
       window.open(`https://www.${query}.com/`);
     } else if (finalText.includes("OK")) {
       replay = "OK";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      addMessageToChat(replay, "received");
       speak(replay);
     } else if (finalText.includes("REFRESH")) {
       replay = "OK refreshing";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      addMessageToChat(replay, "received");
       speak(replay);
       location.reload();
     } else {
       console.log(finalText);
-      replay = "Sorry I can't help.";
-      chatbox.innerHTML +=
-        '<div class="eachmessage received animated"><p>' +
-        replay +
-        "</p></div>";
-      chatbox.scrollTop = chatbox.scrollHeight;
+      replay = "Searching on Google.";
+      addMessageToChat(replay, "received");
       speak(replay);
+      const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(
+        finalText
+      )}`;
+      window.open(searchUrl, "_blank");
+      // location.reload();
+      // replay = "Sorry I can't UnderStand";
+      // replay = generateResponse(p, finalText);
+      // addMessageToChat(replay, "received");
+      // speak(replay);
     }
-    p = document.createElement("p");
   }
 });
 
-function start_animation() {
-  for (let i = 0; i < animate.length; i++) {
-    setTimeout(function () {
-      animate[i].classList.add("animated");
-    }, 300 * i + 300);
-  }
-}
+// function generateResponse(messageElement, userMessage) {
+//   const API_URL = "https://api.openai.com/v1/chat/completions";
+//   const requestOptions = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${api_key2}`,
+//     },
+//     body: JSON.stringify({
+//       model: "gpt-3.5-turbo",
+//       messages: [
+//         {
+//           role: "user",
+//           content: userMessage,
+//         },
+//       ],
+//     }),
+//   };
+
+//   fetch(API_URL, requestOptions)
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return res.json();
+//     })
+//     .then((data) => {
+//       messageElement.textContent = data.choices[0].message.content;
+//       speak(data.choices[0].message.content);
+//     })
+//     .catch((error) => {
+//       messageElement.classList.add("error");
+//       messageElement.textContent =
+//         "Oops! Something went wrong. Please try again!";
+//     })
+//     .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+// }
 
 function getCurrentCity() {
   return new Promise((resolve, reject) => {
@@ -323,7 +232,6 @@ function getCurrentCity() {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          // const apiKey = "YOUR_API_KEY"; // Replace "YOUR_API_KEY" with your actual API key
           const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`;
 
           fetch(url)
@@ -346,4 +254,10 @@ function getCurrentCity() {
   });
 }
 
-// https://www.google.com/search?q=who+is+the+prime+minister+of+india&rlz=1C1UEAD_enIN971IN971&sxsrf=AOaemvI4v5MP8xENaQFFNxmqkFe1ltIKtA%3A1637087940432&ei=xPqTYcvoGc_rwQOs1JTADQ&oq=who+is+the+prime&gs_lcp=Cgdnd3Mtd2l6EAEYADIICAAQgAQQsQMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgjEOoCECc6DAgjECcQnQIQRhCAAjoECCMQJzoFCAAQkQI6BwgAELEDEEM6BAgAEEM6DQgAEIAEELEDEEYQ-wFKBAhBGABQ5hpY2z1g1FBoAXACeACAAa0DiAGxFZIBCjAuMTQuMS4wLjGYAQCgAQGwAQrAAQE&sclient=gws-wiz
+function addMessageToChat(message, role) {
+  const div = document.createElement("div");
+  div.classList.add("eachmessage", role, "animated");
+  div.innerHTML = "<p>" + message + "</p>";
+  chatbox.appendChild(div);
+  chatbox.scrollTop = chatbox.scrollHeight;
+}
